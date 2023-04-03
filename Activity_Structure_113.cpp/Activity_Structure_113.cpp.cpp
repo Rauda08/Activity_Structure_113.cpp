c1c4497820b24247a6832efe9abec71279922334
rauda08 <raudajnnh08@gmail.com>
2023-04-03 12:03:55 +0700

    menambahkan mahasiswa 2

--- Activity_Structure_113.cpp/Activity_Structure_113.cpp.cpp
+++ Activity_Structure_113.cpp/Activity_Structure_113.cpp.cpp
@@ -19,6 +19,13 @@ int main() {
     mhs1.umur = 20;
     
     cout << "Masukan NIM :";
+    cin >> mhs2.NIM;
+    cout << "Masukan Nama :";
+    cin >> mhs2.nama;
+    cout << "Masukan Alamat :";
+    cin >> mhs2.alamat;
+    cout << "Masukan Umur: ";
+    cin >> mhs2.umur;
 
 }
 {
