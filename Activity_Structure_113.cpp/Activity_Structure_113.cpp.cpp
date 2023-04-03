e7f04a4b3be85ac7ad3e6d6975fb3634f5ca68cf
rauda08 <raudajnnh08@gmail.com>
2023-04-03 12:12:31 +0700

    menambahkan tanda ";" pada line 7

--- Activity_Structure_113.cpp/Activity_Structure_113.cpp.cpp
+++ Activity_Structure_113.cpp/Activity_Structure_113.cpp.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 
+
 struct mahasiswa
 {
-    string NIM
-        string nama;
+    string NIM;
+    string nama;
     string alamat;
     int umur;
 };
@@ -17,7 +18,7 @@ int main() {
     mhs1.nama = "Raudatul Jannah";
     mhs1.alamat = "Pangkalan Kerinci";
     mhs1.umur = 20;
-    
+
     cout << "Masukan NIM :";
     cin >> mhs2.NIM;
     cout << "Masukan Nama :";
@@ -36,20 +37,4 @@ int main() {
     cout << "\nNama = " << mhs2.nama;
     cout << "\nAlamat = " << mhs2.alamat;
     cout << "\nUmur = " << mhs2.umur;
-
-
 }
-{
-    std::cout << "Hello World!\n";
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
